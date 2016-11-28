9769384fc552bd5ebdcef4f1454b4d9f33f33a44
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-11-27 16:21:54 -0800

    Header menu

--- functions.php
+++ functions.php
@@ -173,6 +173,10 @@ function hume_scripts() {
 	wp_enqueue_style( 'hume-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'hume-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_localize_script( 'hume-navigation', 'humeScreenReaderText', array(
+		'expand' => __( 'Expand child menu', 'rawls' ),
+		'collapse' => __( 'Next', 'rawls' )
+	) );
 
 	wp_enqueue_script( 'hume-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'hume' ); ?></a>
 	
-	<?php if ( get_header_image() ) : ?>
+	<?php if ( get_header_image() && is_front_page() ) : ?>
 	<figure class="header-image">
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 			<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
@@ -53,7 +53,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hume' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'hume' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
