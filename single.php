1c49439975b6ddfe56d9759a870eec5d3a4c2684
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-11-27 23:58:21 -0800

    Post nav

--- single.php
+++ single.php
@@ -17,7 +17,15 @@ get_header(); ?>
 
 			get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post_navigation();
+			// Previous/next post navigation.
+			the_post_navigation( array(
+				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'hume' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Next post:', 'hume' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'hume' ) . '</span> ' .
+					'<span class="screen-reader-text">' . __( 'Previous post:', 'hume' ) . '</span> ' .
+					'<span class="post-title">%title</span>',
+			) );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
