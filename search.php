1709df3ad4f7b360e6c6bf3bdaa96b004249779e
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2016-12-02 16:13:08 -0800

    various

--- search.php
+++ search.php
@@ -28,7 +28,7 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content' );
 
 			endwhile;
 
